7b90a43cda3b9f1ab082ce91252987bafb7c67fb
Mark Skroba <mark.skroba@slu.edu>
2023-04-27 22:53:15 -0500

    cleanup after merge + autostart

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,5 +6,10 @@ feh --bg-fill ~/Pictures/wall/gruv.png &
 xset r rate 200 50 &
 picom &
 
+# autostart
+xrandr --auto --output HDMI-1-1 --mode 1920x1080 --right-of HDMI-0 &
+nitrogen --restore &
+setxkbmap -layout us,ru -option grp:alt_shift_toggle &
+
 ~/.config/chadwm/scripts/bar.sh &
 while type dwm >/dev/null; do dwm && continue || break; done
